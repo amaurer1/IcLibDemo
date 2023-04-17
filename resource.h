72397d277ac3fbe4744c8d6239ffeb92526a751f
Adrian Maurer <mail@uniograph.com>
2023-04-17 15:38:08 +0200

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by IcLibDemo.rc
+//
+#define IDR_MAIN_FRAME                  100
+#define IDD_ABOUT_DLG                   300
+#define IDI_CURVE_TYPE                  301
+#define IDI_ITERATION_COUNT             302
+#define ID_FILE_SAVE_IMAGE_AS           40000
+#define ID_CURVE_TYPE_LINE_SEGMENTS     40010
+#define ID_CURVE_TYPE_CUBIC_SPLINE      40011
+#define ID_CURVE_TYPE_FINITE_SPLINE     40012
+#define ID_VIEW_GRID                    40020
+#define ID_VIEW_DATA_POINTS             40021
+#define ID_VIEW_CONTROL_POINTS          40022
+#define ID_VIEW_LABELS                  40023
+#define ID_VIEW_CURVE                   40024
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         40032
+#define _APS_NEXT_CONTROL_VALUE         2000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
